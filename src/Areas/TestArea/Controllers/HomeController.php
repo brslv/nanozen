31e1c5bdcf0b9950368bbe3f1db0cc9e946910b5
brslv <brslvg@gmail.com>
2015-09-23 13:09:22 +0300

    add simple testing action to the TestArea's HomeController.

--- src/Areas/TestArea/Controllers/HomeController.php
+++ src/Areas/TestArea/Controllers/HomeController.php
@@ -18,4 +18,9 @@ class HomeController extends BaseController
 		echo "It's the TestArea's HomeController::index method. And it should work by default on /test.";
 	}
 
+	public function welcome()
+	{
+		echo "Welcome to the TestArea's HomeController::welcome().";
+	}
+
 }
