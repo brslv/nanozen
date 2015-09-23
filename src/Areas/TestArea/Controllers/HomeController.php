934bcba5681c784d21cca0cb1654b013d772f8dc
brslv <brslvg@gmail.com>
2015-09-23 13:05:48 +0300

    final refactoring before merge.

--- src/Areas/TestArea/Controllers/HomeController.php
+++ src/Areas/TestArea/Controllers/HomeController.php
@@ -4,17 +4,18 @@ namespace Nanozen\Areas\TestArea\Controllers;
 
 use Nanozen\Providers\Controller\BaseControllerProvider as BaseController;
 
+/**
+ * Class HomeController
+ *
+ * @author brslv
+ * @package Nanozen\Areas\TestArea\Controllers
+ */
 class HomeController extends BaseController 
 {
 
 	public function index()
 	{
 		echo "It's the TestArea's HomeController::index method. And it should work by default on /test.";
-	}
-
-	public function welcome() 
-	{
-		echo 'A simple welcomming message.';
 	}
 
 }
--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@
  * Provide routes for the app.
  * 
  */
+
 // Example for adding custom patterns.
 // The alias should start with :.
 // The pattern should use #.
@@ -11,7 +12,6 @@
 
 // When this route is not available
 // the routing engine will default to HomeController::index.
-
 $router->get('/', 'HomeController@welcome'); 
 
 $router->get('aloha/{name:s}', 'HomeController@aloha');
@@ -20,14 +20,14 @@ $router->get('bye', 'HomeController@bye');
 
 $router->get('nice', 'TestController@nice');
 
+// When this route is available
+// it will override the default behaviour
+// of HomeController::automatic action and will be called on route /auto instead.
+// $router->get('auto', 'HomeController@automatic');
+
 // Areas
 // This simple line activates the routing for areas.
 // By default, the routing is automatic (mapping automatic Controller/action/params).
 // Can be customized by:
 // 		$router->forArea('test')->get('some-route', 'SomeController@action');
 $router->area('test', 'TestArea');
-
-// When this route is available
-// it will override the default behaviour
-// of HomeController::automatic action and will be called on route /auto instead.
-// $router->get('auto', 'HomeController@automatic');
